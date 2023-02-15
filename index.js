ffdc4a10c7080a40e41be57013014b0ff2618a41
Bassam Belaid <95133089+bassam2b2s@users.noreply.github.com>
2023-02-15 07:50:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggleb = document.getElementsByClassName('toggle-b')[0]
+const navlinks = document.getElementsByClassName('nav-links')[0]
+
+toggleb.addEventListener('click', () => {
+navlinks.classList.toggle('active')
+})
